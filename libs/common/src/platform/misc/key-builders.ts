c1f36993d0803c43f8df158789f429c831ed549f
Matt Gibson <mgibson@bitwarden.com>
2023-10-17 17:29:16 -0400

    Prefer opaque types to generics

--- libs/common/src/platform/misc/key-builders.ts
+++ libs/common/src/platform/misc/key-builders.ts
@@ -1,14 +1,18 @@
+import { Opaque } from "type-fest";
+
 import { KeyDefinition } from "../state/key-definition";
 
+export type StorageKey = Opaque<string, "StorageKey">;
+
 // TODO: Use Matts `UserId` type
-export function userKeyBuilder(userId: string, keyDefinition: KeyDefinition<unknown>): string {
+export function userKeyBuilder(userId: string, keyDefinition: KeyDefinition<unknown>): StorageKey {
   if (userId == null) {
     throw new Error("You cannot build a user key without");
   }
-  return `${keyDefinition.stateDefinition.name}_${userId}_${keyDefinition.key}`;
+  return `${keyDefinition.stateDefinition.name}_${userId}_${keyDefinition.key}` as StorageKey;
 }
 
-export function globalKeyBuilder(keyDefinition: KeyDefinition<unknown>): string {
+export function globalKeyBuilder(keyDefinition: KeyDefinition<unknown>): StorageKey {
   // TODO: Do we want the _global_ part?
-  return `${keyDefinition.stateDefinition.name}_global_${keyDefinition.key}`;
+  return `${keyDefinition.stateDefinition.name}_global_${keyDefinition.key}` as StorageKey;
 }
