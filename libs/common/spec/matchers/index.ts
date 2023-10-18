9b8985ac2b0e2a0d81fb088c2cb00223340045c7
Matt Gibson <mgibson@bitwarden.com>
2023-10-18 06:40:23 -0400

    Remove unused matchers

--- libs/common/spec/matchers/index.ts
+++ libs/common/spec/matchers/index.ts
@@ -1,2 +1,1 @@
 export * from "./to-equal-buffer";
-export * from "./to-emit";
--- libs/common/test.setup.ts
+++ libs/common/test.setup.ts
@@ -1,6 +1,6 @@
 import { webcrypto } from "crypto";
 
-import { toEmit, toEmitValue, toEqualBuffer } from "./spec";
+import { toEqualBuffer } from "./spec";
 
 Object.defineProperty(window, "crypto", {
   value: webcrypto,
@@ -10,16 +10,8 @@ Object.defineProperty(window, "crypto", {
 
 expect.extend({
   toEqualBuffer: toEqualBuffer,
-  toEmit: toEmit,
-  toEmitValue: toEmitValue,
 });
 
 export interface CustomMatchers<R = unknown> {
   toEqualBuffer(expected: Uint8Array | ArrayBuffer): R;
-  toEmit(timeoutMs?: number): R;
-  toEmitValue(
-    expected: unknown,
-    comparer?: (a: unknown, b: unknown) => boolean,
-    timeoutMs?: number
-  ): R;
 }
