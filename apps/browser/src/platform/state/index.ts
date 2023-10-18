9d2bc9042503ba7b7c68c751dc836c874c9dc24a
Matt Gibson <mgibson@bitwarden.com>
2023-10-18 12:36:51 -0400

    Remove unused state-level syncing

--- apps/browser/src/platform/state/index.ts
+++
@@ -1,2 +0,0 @@
-export { BackgroundUserStateProvider } from "./background-user-state.provider";
-export { ForegroundUserStateProvider } from "./foreground-user-state.provider";
--- apps/browser/src/platform/state/port-builder.ts
+++
@@ -1,5 +0,0 @@
-import { KeyDefinition } from "@bitwarden/common/platform/state";
-
-export function portNameBuilder(keyDefinition: KeyDefinition<unknown>, scope: "user" | "global") {
-  return `${keyDefinition.stateDefinition.name}_${scope}_${keyDefinition.key}_port`;
-}
