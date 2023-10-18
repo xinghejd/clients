d4cdbb664fc70d8f888035984e0b587d45e43f42
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2023-10-18 15:32:19 -0400

    Add Barrel File for state
    Co-authored-by: Matt Gibson <MGibson1@users.noreply.github.com>

---
+++ libs/common/src/platform/state/index.ts
@@ -0,0 +1,4 @@
+export { DerivedStateDefinition } from "./derived-state-definition";
+export { DerivedUserState } from "./derived-user-state";
+export { KeyDefinition } from "./key-definition";
+export { StorageLocation } from "./state-definition";
