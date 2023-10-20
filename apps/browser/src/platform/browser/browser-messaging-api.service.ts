6aa7aa75cb13bcd8300e1c0a83c8b4b488ad6ee6
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-10-20 15:36:43 +0200

    [PM-4401] feat: use new service

--- apps/browser/src/platform/browser/browser-messaging-api.service.ts
+++ apps/browser/src/platform/browser/browser-messaging-api.service.ts
@@ -4,6 +4,16 @@ import { Observable } from "rxjs";
 import { BrowserApi } from "./browser-api";
 import { runInsideAngular } from "./run-inside-angular.operator";
 
+/**
+ * This service is used to listening to messages from the background script.
+ * It automatically runs all callbacks inside the Angular zone.
+ * This should be used instead of `BrowserApi.messageListener` in all popup-components.
+ * Not needed for services running in the background script.
+ *
+ * Technically this shouldn't be needed because we have a polyfill for `chrome.runtime.sendMessage`
+ * that should run all callbacks inside the Angular zone, but it's currently unstable in Safari,
+ * and only working sometimes.
+ */
 @Injectable({ providedIn: "root" })
 export class BrowserMessagingApiService {
   constructor(private ngZone: NgZone) {}
