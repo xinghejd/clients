a8e51cbd8ade2622be07fead92f1cbbc3c5023b8
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2023-10-20 14:08:17 -0400

    Use Real Placeholder Text

--- bitwarden_license/bit-web/src/app/secrets-manager/settings/importers/bitwarden-json.importer.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/settings/importers/bitwarden-json.importer.ts
@@ -7,7 +7,21 @@ export class BitwardenJsonSecretsManagerImporter extends JsonSecretsManagerImpor
       "Bitwarden (json)",
       JSON.stringify(
         {
-          something: true,
+          projects: [
+            {
+              id: "92ab8d56-73c9-4e4f-8b47-b0a100f1ad0b",
+              name: "Example Project",
+            },
+          ],
+          secrets: [
+            {
+              key: "MySecret",
+              value: "MyValue!",
+              note: "",
+              id: "6e590482-32a9-4e88-9960-466a4be2f105",
+              projectIds: [],
+            },
+          ],
         },
         null,
         2
