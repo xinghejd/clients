4e051d5f17911009361bc66d2e01168cb22d8f92
rr-bw <102181210+rr-bw@users.noreply.github.com>
2023-10-30 16:01:04 -0700

    add avatar and simple navigation to header

--- apps/browser/src/auth/popup/account-switching/account-switcher.component.ts
+++ apps/browser/src/auth/popup/account-switching/account-switcher.component.ts
@@ -1,6 +1,10 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 
+import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
+// import { AuthenticationStatus } from "@bitwarden/common/auth/enums/authentication-status";
+// import { UserId } from "@bitwarden/common/types/guid";
+
 import { BrowserRouterService } from "../../../platform/popup/services/browser-router.service";
 import { AccountSwitcherService } from "../services/account-switcher.service";
 
@@ -11,8 +15,21 @@ export class AccountSwitcherComponent {
   constructor(
     private accountSwitcherService: AccountSwitcherService,
     private router: Router,
-    private routerService: BrowserRouterService
-  ) {}
+    private routerService: BrowserRouterService,
+    private accountService: AccountService
+  ) {
+    // this.accountService.addAccount("Mario" as UserId, {
+    //   name: "Mario",
+    //   email: "mario@test.com",
+    //   status: AuthenticationStatus.Locked,
+    // });
+    // this.accountService.addAccount("Yoshi" as UserId, {
+    //   name: "Yoshi",
+    //   email: "yoshi@test.com",
+    //   status: AuthenticationStatus.Locked,
+    // });
+    // this.accountService.switchAccount("Mario" as UserId);
+  }
 
   get accountOptions$() {
     return this.accountSwitcherService.accountOptions$;
