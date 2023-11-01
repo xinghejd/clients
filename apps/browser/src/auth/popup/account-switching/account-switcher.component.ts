47c26749717d1494d39b6d57c13abf828cd55e67
rr-bw <102181210+rr-bw@users.noreply.github.com>
2023-11-01 12:29:29 -0700

    add options buttons

--- apps/browser/src/auth/popup/account-switching/account-switcher.component.ts
+++ apps/browser/src/auth/popup/account-switching/account-switcher.component.ts
@@ -2,8 +2,8 @@ import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 
 import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
-// import { AuthenticationStatus } from "@bitwarden/common/auth/enums/authentication-status";
-// import { UserId } from "@bitwarden/common/types/guid";
+import { AuthenticationStatus } from "@bitwarden/common/auth/enums/authentication-status";
+import { UserId } from "@bitwarden/common/types/guid";
 
 import { BrowserRouterService } from "../../../platform/popup/services/browser-router.service";
 import { AccountSwitcherService } from "../services/account-switcher.service";
@@ -16,19 +16,22 @@ export class AccountSwitcherComponent {
     private accountSwitcherService: AccountSwitcherService,
     private router: Router,
     private routerService: BrowserRouterService,
+
     private accountService: AccountService
   ) {
-    // this.accountService.addAccount("Mario" as UserId, {
-    //   name: "Mario",
-    //   email: "mario@test.com",
-    //   status: AuthenticationStatus.Locked,
-    // });
-    // this.accountService.addAccount("Yoshi" as UserId, {
-    //   name: "Yoshi",
-    //   email: "yoshi@test.com",
-    //   status: AuthenticationStatus.Locked,
-    // });
-    // this.accountService.switchAccount("Mario" as UserId);
+    this.accountService.addAccount("Mario" as UserId, {
+      name: "Mario",
+      email: "mario@test.com",
+      status: AuthenticationStatus.Locked,
+    });
+
+    this.accountService.addAccount("Yoshi" as UserId, {
+      name: "Yoshi",
+      email: "yoshi@test.com",
+      status: AuthenticationStatus.Locked,
+    });
+
+    this.accountService.switchAccount("Mario" as UserId);
   }
 
   get accountOptions$() {
