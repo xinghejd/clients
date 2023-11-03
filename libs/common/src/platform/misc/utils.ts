eb0671f5c27d59e001666d1f2cdbffdefc6a961f
Matt Bishop <mbishop@bitwarden.com>
2023-11-03 12:36:32 -0400

    Relax UUID validation

--- libs/common/src/platform/misc/utils.ts
+++ libs/common/src/platform/misc/utils.ts
@@ -223,7 +223,7 @@ export class Utils {
 
   static isGuid(id: string) {
     return RegExp(
-      /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/,
+      /^[0-9a-f]{8}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{12}$/,
       "i"
     ).test(id);
   }
