b59df6da18789a9328216c893569f4be41cc16ff
Matt Gibson <mgibson@bitwarden.com>
2023-11-14 11:54:53 -0500

    Prefer bound arrow functions

--- libs/common/src/platform/state/key-definitions.ts
+++ libs/common/src/platform/state/key-definitions.ts
@@ -1,3 +1,4 @@
+import { AccountInfo } from "../../auth/abstractions/account.service";
 import { AccountsDeserializer } from "../../auth/services/account.service";
 import { UserId } from "../../types/guid";
 
@@ -5,9 +6,13 @@ import { KeyDefinition } from "./key-definition";
 import { StateDefinition } from "./state-definition";
 
 const ACCOUNT_MEMORY = new StateDefinition("account", "memory");
-export const ACCOUNT_ACCOUNTS = new KeyDefinition(ACCOUNT_MEMORY, "accounts", {
-  deserializer: AccountsDeserializer,
-});
+export const ACCOUNT_ACCOUNTS = new KeyDefinition<Record<UserId, AccountInfo>>(
+  ACCOUNT_MEMORY,
+  "accounts",
+  {
+    deserializer: (obj) => AccountsDeserializer(obj),
+  }
+);
 export const ACCOUNT_ACTIVE_ACCOUNT_ID = new KeyDefinition(ACCOUNT_MEMORY, "activeAccountId", {
   deserializer: (id: UserId) => id,
 });
