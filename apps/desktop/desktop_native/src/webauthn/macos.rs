52c95465724ef7aaa637d546a478d0a999a14d9f
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-12-08 13:59:25 +0100

    [PM-4060] non-working delegate with `declare_class!`

--- apps/desktop/desktop_native/src/webauthn/macos.rs
+++ apps/desktop/desktop_native/src/webauthn/macos.rs
@@ -1,54 +1,93 @@
 use anyhow::Result;
 use icrate::{
     objc2::{
-        declare_class, extern_methods, mutability::InteriorMutable, rc::Id, ClassType,
-        DeclaredClass,
+        declare_class, msg_send_id,
+        mutability::{self, InteriorMutable},
+        rc::{Allocated, Id},
+        runtime::ProtocolObject,
+        ClassType, DeclaredClass, Encoding, RefEncode,
     },
-    AppKit::NSWindow,
-    AuthenticationServices::ASAuthorizationPlatformPublicKeyCredentialProvider,
+    AppKit::{NSApplication, NSWindow},
+    AuthenticationServices::{
+        ASAuthorization, ASAuthorizationController, ASAuthorizationControllerDelegate,
+        ASAuthorizationControllerPresentationContextProviding,
+        ASAuthorizationPlatformPublicKeyCredentialProvider, ASAuthorizationRequest,
+        ASPresentationAnchor,
+    },
     Foundation::{
-        ns_string, NSData, NSDataBase64DecodingIgnoreUnknownCharacters, NSObject, NSString,
+        ns_string, MainThreadMarker, NSArray, NSData, NSDataBase64DecodingIgnoreUnknownCharacters,
+        NSError, NSObject, NSObjectProtocol, NSString,
     },
 };
 
 pub fn create(_window_handle: u64) -> Result<String> {
-    let rp_id = ns_string!("shiny.coroiu.com"); // Example of how to create static "string literal" NSString
-    let credential_provider = unsafe {
-        ASAuthorizationPlatformPublicKeyCredentialProvider::initWithRelyingPartyIdentifier(
-            ASAuthorizationPlatformPublicKeyCredentialProvider::alloc(),
-            rp_id,
-        )
-    };
+    MainThreadMarker::run_on_main(|mtm| {
+        let rp_id = ns_string!("shiny.coroiu.com"); // Example of how to create static "string literal" NSString
+        let credential_provider = unsafe {
+            ASAuthorizationPlatformPublicKeyCredentialProvider::initWithRelyingPartyIdentifier(
+                ASAuthorizationPlatformPublicKeyCredentialProvider::alloc(),
+                rp_id,
+            )
+        };
 
-    let user_name = NSString::from_str("user"); // Example of how to create dynamic NSString
-    let challenge = unsafe {
-        NSData::initWithBase64EncodedString_options(
-            NSData::alloc(),
-            ns_string!("YW5kcmVhcyBjb3JvaXUgd2FzIGhlcmU="),
-            NSDataBase64DecodingIgnoreUnknownCharacters,
-        )
-    };
-    let user_id = unsafe {
-        NSData::initWithBase64EncodedString_options(
-            NSData::alloc(),
-            ns_string!("6dnpKhnpRAS4diyuFwS+Rg=="), // e9d9e92a-19e9-4404-b876-2cae1704be46 in base64
-            NSDataBase64DecodingIgnoreUnknownCharacters,
-        )
-    };
+        let user_name = NSString::from_str("user"); // Example of how to create dynamic NSString
+        let challenge = unsafe {
+            NSData::initWithBase64EncodedString_options(
+                NSData::alloc(),
+                ns_string!("YW5kcmVhcyBjb3JvaXUgd2FzIGhlcmU="),
+                NSDataBase64DecodingIgnoreUnknownCharacters,
+            )
+        };
+        let user_id = unsafe {
+            NSData::initWithBase64EncodedString_options(
+                NSData::alloc(),
+                ns_string!("6dnpKhnpRAS4diyuFwS+Rg=="), // e9d9e92a-19e9-4404-b876-2cae1704be46 in base64
+                NSDataBase64DecodingIgnoreUnknownCharacters,
+            )
+        };
 
-    let registration_request = unsafe {
-        credential_provider.createCredentialRegistrationRequestWithChallenge_name_userID(
-            challenge.unwrap().as_ref(),
-            user_name.as_ref(),
-            user_id.unwrap().as_ref(),
-        )
-    };
+        let platform_registration_request = unsafe {
+            credential_provider.createCredentialRegistrationRequestWithChallenge_name_userID(
+                challenge.unwrap().as_ref(),
+                user_name.as_ref(),
+                user_id.unwrap().as_ref(),
+            )
+        };
 
-    println!("registration_request: {:?}", registration_request);
+        println!("registration_request: {:?}", platform_registration_request);
+
+        let main_window = unsafe {
+            NSApplication::sharedApplication(mtm)
+                .windows()
+                .objectAtIndex(0)
+        };
+        let auth_delegate = AuthDelegate::init(AuthDelegate::alloc(), main_window);
+
+        // let authorization_requests = unsafe {
+        //     NSArray::<Id<ASAuthorizationPlatformPublicKeyCredentialRegistrationRequest>>::arrayWithObject(registration_request)
+        // };
+        let registration_request = platform_registration_request.as_super().retain();
+        let authorization_requests =
+            NSArray::<ASAuthorizationRequest>::from_vec(vec![registration_request]);
+        let auth_controller = unsafe {
+            ASAuthorizationController::initWithAuthorizationRequests(
+                ASAuthorizationController::alloc(),
+                authorization_requests.as_ref(),
+            )
+        };
+        let unwrapperd_delegate = auth_delegate.unwrap();
+
+        let auth_controller_delegate = ProtocolObject::from_ref(&*unwrapperd_delegate);
+        unsafe { auth_controller.setDelegate(Some(auth_controller_delegate)) };
+
+        let presentation_context_delegate = ProtocolObject::from_ref(&*unwrapperd_delegate);
+        unsafe { auth_controller.setPresentationContextProvider(presentation_context_delegate) };
+    });
 
     Ok("not implemented".to_owned())
 }
 
+#[allow(unused)]
 struct AuthDelegateIvars {
     window: Id<NSWindow>,
 }
@@ -58,7 +97,8 @@ declare_class!(
 
     unsafe impl ClassType for AuthDelegate {
         type Super = NSObject;
-        type Mutability = InteriorMutable;
+        // type Mutability = InteriorMutable;
+        type Mutability = mutability::MainThreadOnly;
         const NAME: &'static str = "AuthDelegate";
     }
 
@@ -66,16 +106,52 @@ declare_class!(
         type Ivars = AuthDelegateIvars;
     }
 
-//     unsafe impl MyObject {
-//         #[method_id(init)]
-//         pub fn init(this: Allocated<Self>) -> Option<Id<Self>> {
-//             let this = this.set_ivars(MyIvars {
-//                 object: NSObject::new(),
-//             });
-//             unsafe { msg_send_id![super(this), init] }
-//         }
-//     }
+    unsafe impl NSObjectProtocol for AuthDelegate {}
+
+    unsafe impl ASAuthorizationControllerDelegate for AuthDelegate {
+        #[allow(non_snake_case)]
+        #[method(authorizationController:didCompleteWithAuthorization:)]
+        unsafe fn authorizationController_didCompleteWithAuthorization(
+            &self,
+            controller: &ASAuthorizationController,
+            authorization: &ASAuthorization,
+        ) {
+            println!("didCompleteWithAuthorization SUCCESS");
+        }
+
+        #[allow(non_snake_case)]
+        #[method(authorizationController:didCompleteWithError:)]
+        unsafe fn authorizationController_didCompleteWithError(
+            &self,
+            controller: &ASAuthorizationController,
+            error: &NSError,
+        ) {
+            println!("didCompleteWithError FAIL");
+        }
+    }
+
+    unsafe impl ASAuthorizationControllerPresentationContextProviding for AuthDelegate {
+        #[allow(non_snake_case)]
+        #[method(presentationAnchorForAuthorizationController:)]
+        unsafe fn presentationAnchorForAuthorizationController(
+            &self,
+            controller: &ASAuthorizationController,
+        ) -> Id<ASPresentationAnchor> {
+            self.ivars().window
+        }
+    }
 );
+
+// unsafe impl RefEncode for Id<ASPresentationAnchor> {
+//     const ENCODING_REF: Encoding = Encoding::Object;
+// }
+
+impl AuthDelegate {
+    pub fn init(this: Allocated<Self>, window: Id<NSWindow>) -> Option<Id<Self>> {
+        let this = this.set_ivars(AuthDelegateIvars { window });
+        unsafe { msg_send_id![super(this), init] }
+    }
+}
 
 // extern_methods!(
 //     unsafe impl MyObject {
