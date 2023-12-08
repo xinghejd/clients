0629681e9e72ba645ecdaca64877f6f20ed5f968
Cesar Gonzalez <cgonzalez@bitwarden.com>
2023-12-08 08:03:16 -0600

    [PM-4641] Setting up separate folder for browser-mv3

--- apps/browser-mv3/src/auth/popup/services/unauth-guard.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from "@angular/core";
-
-import { UnauthGuard as BaseUnauthGuardService } from "@bitwarden/angular/auth/guards";
-
-@Injectable()
-export class UnauthGuardService extends BaseUnauthGuardService {
-  protected homepage = "tabs/current";
-}
