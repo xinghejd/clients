6883d2d215fa760119f5b71e7f39ece9581d0727
Cesar Gonzalez <cgonzalez@bitwarden.com>
2024-01-04 10:03:58 -0600

    Fixing caching of dependencies using service factories

--- apps/browser/src/platform/background/service-factories/factory-options.ts
+++ apps/browser/src/platform/background/service-factories/factory-options.ts
@@ -6,23 +6,26 @@ export type FactoryOptions = {
   [optionsKey: string]: unknown;
 };
 
+const cachedServices: CachedServices = {};
+
 export async function factory<
   TCache extends CachedServices,
-  TName extends keyof TCache,
+  TName extends keyof CachedServices,
   TOpts extends FactoryOptions,
 >(
-  cachedServices: TCache,
+  UNUSED_cachedServices: TCache, // TODO: CG - This needs to be removed and updated within all calls to the factory method
   name: TName,
   opts: TOpts,
-  factory: () => TCache[TName] | Promise<TCache[TName]>,
-): Promise<TCache[TName]> {
+  factory: () => CachedServices[TName] | Promise<CachedServices[TName]>,
+): Promise<CachedServices[TName]> {
   let instance = cachedServices[name];
+  const instancePreviouslyInitialized = !!instance;
   if (opts.alwaysInitializeNewService || !instance) {
     const instanceOrPromise = factory();
     instance = instanceOrPromise instanceof Promise ? await instanceOrPromise : instanceOrPromise;
   }
 
-  if (!opts.doNotStoreInitializedService) {
+  if (!opts.doNotStoreInitializedService && !instancePreviouslyInitialized) {
     cachedServices[name] = instance;
   }
 
