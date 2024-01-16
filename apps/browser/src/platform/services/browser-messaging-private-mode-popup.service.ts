32936ef263509c1cd94d25776bfa762243351f77
Cesar Gonzalez <cgonzalez@bitwarden.com>
2024-01-16 13:43:32 -0600

    Fixing issue with messaging happening internally

--- apps/browser/src/platform/services/browser-messaging-private-mode-popup.service.ts
+++ apps/browser/src/platform/services/browser-messaging-private-mode-popup.service.ts
@@ -2,7 +2,9 @@ import { MessagingService } from "@bitwarden/common/platform/abstractions/messag
 
 export default class BrowserMessagingPrivateModePopupService implements MessagingService {
   send(subscriber: string, arg: any = {}) {
-    const message = Object.assign({}, { command: subscriber }, arg);
-    (window as any).bitwardenBackgroundMessageListener(message);
+    chrome.runtime.sendMessage({ command: subscriber, ...arg }); // TODO CG - This needs to be investigated, currently it sends in a local capacity, not sure if that's valid.
+
+    // const message = Object.assign({}, { command: subscriber }, arg);
+    // (globalThis as any).bitwardenBackgroundMessageListener(message);
   }
 }
