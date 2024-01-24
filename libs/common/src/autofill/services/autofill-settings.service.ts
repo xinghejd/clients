51fd288a8d3b3b0012c7bd79d808032adb0639c2
Jonathan Prusik <jprusik@classynemesis.com>
2024-01-24 10:22:20 -0500

    update autofill settings service

--- libs/common/src/autofill/services/autofill-settings.service.ts
+++ libs/common/src/autofill/services/autofill-settings.service.ts
@@ -1,28 +1,26 @@
 import { Observable, map } from "rxjs";
 
-import { LogService } from "../../platform/abstractions/log.service";
 import {
   AUTOFILL_SETTINGS_DISK,
   ActiveUserState,
-  StateProvider,
   KeyDefinition,
+  StateProvider,
 } from "../../platform/state";
 
 const AUTOFILL_ON_PAGE_LOAD = new KeyDefinition(AUTOFILL_SETTINGS_DISK, "autoFillOnPageLoad", {
   deserializer: (value: boolean) => value ?? false,
 });
 
-export abstract class AutofillSettingsServiceAbstraction {}
+export abstract class AutofillSettingsServiceAbstraction {
+  autofillOnLoad$: Observable<boolean>;
+  setAutofillOnPageLoad: (newValue: boolean) => Promise<void>;
+}
 
-export class AutofillSettingsService {
+export class AutofillSettingsService implements AutofillSettingsServiceAbstraction {
   private autofillOnLoadState: ActiveUserState<boolean>;
-
   readonly autofillOnLoad$: Observable<boolean>;
 
-  constructor(
-    private logService: LogService,
-    private stateProvider: StateProvider,
-  ) {
+  constructor(private stateProvider: StateProvider) {
     this.autofillOnLoadState = this.stateProvider.getActive(AUTOFILL_ON_PAGE_LOAD);
     this.autofillOnLoad$ = this.autofillOnLoadState.state$.pipe(map((x) => x ?? false));
   }
