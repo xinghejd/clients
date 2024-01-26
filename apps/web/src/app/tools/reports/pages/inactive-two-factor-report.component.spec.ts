1a018cc6b6d3f952db0b316743fa4f97c1affbe1
Vincent Salucci <vincesalucci21@gmail.com>
2024-01-26 15:32:19 -0600

    fix: tests for inactive two factor component, refs AC-1454

--- apps/web/src/app/tools/reports/pages/inactive-two-factor-report.component.spec.ts
+++ apps/web/src/app/tools/reports/pages/inactive-two-factor-report.component.spec.ts
@@ -5,8 +5,11 @@ import { mock } from "jest-mock-extended";
 import { I18nPipe } from "@bitwarden/angular/platform/pipes/i18n.pipe";
 import { ModalService } from "@bitwarden/angular/services/modal.service";
 import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
+import { ConfigServiceAbstraction } from "@bitwarden/common/platform/abstractions/config/config.service.abstraction";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { LogService } from "@bitwarden/common/platform/abstractions/log.service";
+// eslint-disable-next-line no-restricted-imports
+import { ReportsApiServiceAbstraction } from "@bitwarden/common/tools/reports/reports-api.service.abstraction";
 import { CipherService } from "@bitwarden/common/vault/abstractions/cipher.service";
 import { PasswordRepromptService } from "@bitwarden/vault";
 
@@ -30,6 +33,10 @@ describe("InactiveTwoFactorReportComponent", () => {
           useValue: mock<OrganizationService>(),
         },
         {
+          provide: ConfigServiceAbstraction,
+          useValue: mock<ConfigServiceAbstraction>(),
+        },
+        {
           provide: ModalService,
           useValue: mock<ModalService>(),
         },
@@ -44,6 +51,10 @@ describe("InactiveTwoFactorReportComponent", () => {
         {
           provide: I18nService,
           useValue: mock<I18nService>(),
+        },
+        {
+          provide: ReportsApiServiceAbstraction,
+          useValue: mock<ReportsApiServiceAbstraction>(),
         },
       ],
       schemas: [],
