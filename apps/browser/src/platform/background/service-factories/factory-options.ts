abc0d74406de540cdadbf1927b9eeadd743f0b35
Cesar Gonzalez <cgonzalez@bitwarden.com>
2024-02-06 11:39:04 -0600

    Setting up factory services to cache a global object

--- apps/browser/src/platform/background/service-factories/factory-options.ts
+++ apps/browser/src/platform/background/service-factories/factory-options.ts
@@ -6,23 +6,27 @@ export type FactoryOptions = {
   [optionsKey: string]: unknown;
 };
 
+// CG - This forces a cache to be globally referenced before all factory methods
+const cachedServices: CachedServices = {};
+
 export async function factory<
   TCache extends CachedServices,
-  TName extends keyof TCache,
+  TName extends keyof CachedServices,
   TOpts extends FactoryOptions,
 >(
-  cachedServices: TCache,
+  UNUSED_cachedServices: TCache,
   name: TName,
   opts: TOpts,
   factory: () => TCache[TName] | Promise<TCache[TName]>,
 ): Promise<TCache[TName]> {
   let instance = cachedServices[name];
+  const instancedExists = Boolean(instance);
   if (opts.alwaysInitializeNewService || !instance) {
     const instanceOrPromise = factory();
     instance = instanceOrPromise instanceof Promise ? await instanceOrPromise : instanceOrPromise;
   }
 
-  if (!opts.doNotStoreInitializedService) {
+  if (!opts.doNotStoreInitializedService && !instancedExists) {
     cachedServices[name] = instance;
   }
 
