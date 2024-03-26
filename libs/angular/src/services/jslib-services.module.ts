329c97d25ebb35515cfe757244533d24c0ff9179
Jared Snider <116684653+JaredSnider-Bitwarden@users.noreply.github.com>
2024-03-26 18:41:14 -0400

    Auth/PM-7072 - Token Service - Access Token Secure Storage Refactor (#8412)
    * PM-5263 - TokenSvc - WIP on access token secure storage refactor
    
    * PM-5263 - Add key generation svc to token svc.
    
    * PM-5263 - TokenSvc - more progress on encrypt access token work.
    
    * PM-5263 - TokenSvc TODO cleanup
    
    * PM-5263 - TokenSvc - rename
    
    * PM-5263 - TokenSvc - decryptAccess token must return null as that is a valid case.
    
    * PM-5263 - Add EncryptSvc dep to TokenSvc
    
    * PM-5263 - Add secure storage to token service
    
    * PM-5263 - TokenSvc - (1) Finish implementing accessTokenKey stored in secure storage + encrypted access token stored on disk  (2) Remove no longer necessary migration flag as the presence of the accessTokenKey now serves the same purpose.
    
    Co-authored-by: Jake Fink <jfink@bitwarden.com>
    
    * PM-5263 - TokenSvc - (1) Tweak return structure of decryptAccessToken to be more debuggable (2) Add TODO to add more error handling.
    
    * PM-5263 - TODO: update tests
    
    * PM-5263 - add temp logs
    
    * PM-5263 - TokenSvc - remove logs now that I don't need them.
    
    * fix tests for access token
    
    * PM-5263 - TokenSvc test cleanup - small tweaks / cleanup
    
    * PM-5263 - TokenService - per PR feedback from Justin - add error message to error message if possible.
    
    Co-authored-by: Justin Baur <19896123+justindbaur@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jake Fink <jfink@bitwarden.com>
    Co-authored-by: Justin Baur <19896123+justindbaur@users.noreply.github.com>

--- libs/angular/src/services/jslib-services.module.ts
+++ libs/angular/src/services/jslib-services.module.ts
@@ -502,7 +502,10 @@ const typesafeProviders: Array<SafeProvider> = [
       SingleUserStateProvider,
       GlobalStateProvider,
       SUPPORTS_SECURE_STORAGE,
-      AbstractStorageService,
+      SECURE_STORAGE,
+      KeyGenerationServiceAbstraction,
+      EncryptService,
+      LogService,
     ],
   }),
   safeProvider({
