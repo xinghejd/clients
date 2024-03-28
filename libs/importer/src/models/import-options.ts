546358c754a1b9eeec31ec8558904ffddb4dbeeb
Anders Åberg <anders@andersaberg.com>
2024-03-28 18:41:42 +0100

    Rename to UI to Universal Credential Exchange

--- libs/importer/src/models/import-options.ts
+++ libs/importer/src/models/import-options.ts
@@ -4,7 +4,7 @@ export interface ImportOption {
 }
 
 export const featuredImportOptions = [
-  { id: "creeprequest", name: "CREEP Request" },
+  { id: "creeprequest", name: "Universal Credential Exchange" },
   { id: "CEF", name: "CEF Plaintext import" },
   { id: "bitwardenjson", name: "Bitwarden (json)" },
   { id: "bitwardencsv", name: "Bitwarden (csv)" },
