d5e6a5158eb6c19c5c9b2c8ac854fc77fc216836
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-04-02 16:21:20 -0400

    Migrate State

--- libs/common/src/vault/services/key-state/vault-settings.state.ts
+++ libs/common/src/vault/services/key-state/vault-settings.state.ts
@@ -1,4 +1,4 @@
-import { VAULT_SETTINGS_DISK, KeyDefinition } from "../../../platform/state";
+import { VAULT_SETTINGS_DISK, UserKeyDefinition, KeyDefinition } from "../../../platform/state";
 
 export const USER_ENABLE_PASSKEYS = new KeyDefinition<boolean>(
   VAULT_SETTINGS_DISK,
@@ -8,16 +8,17 @@ export const USER_ENABLE_PASSKEYS = new KeyDefinition<boolean>(
   },
 );
 
-export const SHOW_CARDS_CURRENT_TAB = new KeyDefinition<boolean>(
+export const SHOW_CARDS_CURRENT_TAB = new UserKeyDefinition<boolean>(
   VAULT_SETTINGS_DISK,
   "showCardsCurrentTab",
   {
     deserializer: (obj) => obj,
+    clearOn: [], // User setting, don't clear on lock or logout
   },
 );
 
-export const SHOW_IDENTITIES_CURRENT_TAB = new KeyDefinition<boolean>(
+export const SHOW_IDENTITIES_CURRENT_TAB = new UserKeyDefinition<boolean>(
   VAULT_SETTINGS_DISK,
   "showIdentitiesCurrentTab",
-  { deserializer: (obj) => obj },
+  { deserializer: (obj) => obj, clearOn: [] }, // User setting, don't clear on lock or logout
 );
