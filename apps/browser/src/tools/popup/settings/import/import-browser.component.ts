bc5b1beb1c4c5485fab7ef58d8c90552f61828ad
Hinton <hinton@users.noreply.github.com>
2024-04-02 09:38:44 +0200

    Wip hpke

--- apps/browser/src/tools/popup/settings/import/import-browser.component.ts
+++ apps/browser/src/tools/popup/settings/import/import-browser.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { Router, RouterLink } from "@angular/router";
+import { AeadId, CipherSuite, KdfId, KemId } from "hpke-js";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { AsyncActionsModule, ButtonModule, DialogModule } from "@bitwarden/components";
@@ -45,17 +46,49 @@ export class ImportBrowserComponent {
   }
 
   protected async sendCreepRequest(): Promise<void> {
+    /*this.key = await window.crypto.subtle.generateKey(
+      {
+        name: "AES-GCM",
+        length: 128,
+      },
+      true,
+      ["encrypt", "decrypt"],
+    );
+
+    const pubKey = await window.crypto.subtle.exportKey("jwk", this.key);
+    */
+
+    const suite = new CipherSuite({
+      kem: KemId.DhkemP256HkdfSha256,
+      kdf: KdfId.HkdfSha256,
+      aead: AeadId.Aes128Gcm,
+    });
+
+    const rkp = await suite.kem.generateKeyPair();
+
+    const pubKey = await window.crypto.subtle.exportKey("jwk", rkp.publicKey);
+
     // TODO: Create Valid CREEP Request message
     const requestMessage = {
       type: "creepExportRequest",
       content: {
         version: 0,
-        hpke: [""], // includes public-key
+        hpke: [
+          {
+            mode: "base",
+            kem: KemId.DhkemP256HkdfSha256,
+            kdf: KdfId.HkdfSha256,
+            aead: AeadId.Aes128Gcm,
+            key: pubKey,
+          },
+        ], // includes public-key
         zip: ["zip"],
         importer: "Bitwarden",
         credentialTypes: ["basic-auth"],
       },
     };
+
+    console.log(requestMessage);
 
     await chrome.runtime.sendMessage({
       command: "initiateCreepRequest",
