4377d19b9f5059d4ba64dad509363e5063dbbf8e
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2024-04-11 02:52:13 +1000

    [AC-2436] Show unassigned items banner in web (#8655)
    * Boostrap basic banner, show for all admins
    
    * Remove UI banner, fix method calls
    
    * Invert showBanner -> hideBanner
    
    * Add api call
    
    * Minor tweaks and wording
    
    * Change to active user state
    
    * Add tests
    
    * Fix mixed up names
    
    * Simplify logic
    
    * Add feature flag
    
    * Do not clear on logout
    
    * Update apps/web/src/locales/en/messages.json
    
    ---------
    
    Co-authored-by: Addison Beck <github@addisonbeck.com>
    (cherry picked from commit be362988b01b62621249a6a1bb56cd39f93af9f5)

--- libs/common/src/enums/feature-flag.enum.ts
+++ libs/common/src/enums/feature-flag.enum.ts
@@ -8,6 +8,7 @@ export enum FeatureFlag {
   KeyRotationImprovements = "key-rotation-improvements",
   FlexibleCollectionsMigration = "flexible-collections-migration",
   ShowPaymentMethodWarningBanners = "show-payment-method-warning-banners",
+  UnassignedItemsBanner = "unassigned-items-banner",
 }
 
 // Replace this with a type safe lookup of the feature flag values in PM-2282
