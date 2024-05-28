db01190acb7e0367247a511472661b67f97d213d
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-05-28 14:44:36 +0200

    [CL-298] feat: implement async actions service

--- libs/components/src/async-actions/async-actions.service.spec.ts
+++ libs/components/src/async-actions/async-actions.service.spec.ts
@@ -1,4 +1,13 @@
-import { lastValueFrom } from "rxjs";
+import {
+  Observable,
+  Subject,
+  bufferCount,
+  firstValueFrom,
+  lastValueFrom,
+  map,
+  merge,
+  take,
+} from "rxjs";
 
 import { AsyncActionsService } from "./async-actions.service";
 
@@ -11,9 +20,106 @@ describe("AsyncActionsService", () => {
 
   describe("state$", () => {
     it("emits 'inactive' when no action has been executed", async () => {
-      const state = await lastValueFrom(service.state$("context"));
+      const state = await firstValueFrom(service.state$("context"));
 
       expect(state).toEqual({ status: "inactive" });
     });
+
+    it("emits 'active' when an action has been executed but not yet completed", async () => {
+      const action = new Subject();
+      const origin = Symbol();
+      void service.execute("context", origin, () => action);
+
+      const state = await firstValueFrom(service.state$("context"));
+
+      expect(state).toEqual({ status: "active", origin });
+    });
+
+    it("emits 'inactive' when an action has been executed and completed", async () => {
+      const action = new Subject();
+      const origin = Symbol();
+      const execution = service.execute("context", origin, () => action);
+      action.next(undefined);
+      await execution;
+
+      const state = await firstValueFrom(service.state$("context"));
+
+      expect(state).toEqual({ status: "inactive" });
+    });
+
+    it("emits 'inactive' when an action has been executed and thrown an error", async () => {
+      const action = new Subject();
+      const origin = Symbol();
+      const execution = service.execute("context", origin, () => action);
+      action.error(new Error("test"));
+      await execution.catch(() => {});
+
+      const state = await firstValueFrom(service.state$("context"));
+
+      expect(state).toEqual({ status: "inactive" });
+    });
+
+    it("tracks and emits status of multiple contexts", async () => {
+      const a = { action: new Subject(), origin: Symbol("a") };
+      const b = { action: new Subject(), origin: Symbol("b") };
+      const merged_states = merge(
+        service.state$("context_a").pipe(map((state) => ({ context: "context_a", state }))),
+        service.state$("context_b").pipe(map((state) => ({ context: "context_b", state }))),
+      );
+      const emissions = firstNValuesFrom(6, merged_states);
+
+      const execution_a = service.execute("context_a", a.origin, () => a.action);
+      const execution_b = service.execute("context_b", b.origin, () => b.action);
+      b.action.next(undefined);
+      await execution_b;
+      a.action.next(undefined);
+      await execution_a;
+
+      const output = await emissions;
+      expect(output).toEqual([
+        { context: "context_a", state: { status: "inactive" } },
+        { context: "context_b", state: { status: "inactive" } },
+        { context: "context_a", state: { status: "active", origin: a.origin } },
+        { context: "context_b", state: { status: "active", origin: b.origin } },
+        { context: "context_b", state: { status: "inactive" } },
+        { context: "context_a", state: { status: "inactive" } },
+      ]);
+    });
+  });
+
+  describe("execute", () => {
+    it("executes the handler and re-throws any errors", async () => {
+      const error = new Error("example");
+
+      const result = async () =>
+        await service.execute("context", Symbol(), () => Promise.reject(error));
+
+      await expect(result).rejects.toThrow(error);
+    });
+
+    it("unsubscribes from the observable when the service is destroyed", async () => {
+      const action = new Subject();
+      const origin = Symbol();
+      void service.execute("context", origin, () => action);
+
+      service.ngOnDestroy();
+
+      expect(action.observed).toBe(false);
+    });
+
+    it("unsubscribes from the observable when 'until' emits", async () => {
+      const action = new Subject();
+      const until = new Subject();
+      const origin = Symbol();
+      void service.execute("context", origin, () => action, until);
+
+      until.next(undefined);
+
+      expect(action.observed).toBe(false);
+    });
   });
 });
+
+function firstNValuesFrom<T>(count: number, observable: Observable<T>): Promise<T[]> {
+  return lastValueFrom(observable.pipe(bufferCount(count), take(1)));
+}
--- libs/components/src/async-actions/async-actions.service.ts
+++ libs/components/src/async-actions/async-actions.service.ts
@@ -1,17 +1,44 @@
-import { Injectable } from "@angular/core";
-import { Observable, of } from "rxjs";
+import { Injectable, OnDestroy } from "@angular/core";
+import {
+  BehaviorSubject,
+  EmptyError,
+  Observable,
+  Subject,
+  distinctUntilChanged,
+  firstValueFrom,
+  map,
+  merge,
+  takeUntil,
+} from "rxjs";
 
-import { FunctionReturningAwaitable } from "../utils/function-to-observable";
+import { FunctionReturningAwaitable, functionToObservable } from "../utils/function-to-observable";
 
 export type ContextState = { status: "inactive" } | { status: "active"; origin: unknown };
 export type Status = ContextState["status"];
+export type ServiceState = { [context: string]: ContextState };
 
 @Injectable({
   providedIn: "root",
 })
-export class AsyncActionsService {
+export class AsyncActionsService implements OnDestroy {
+  private readonly onDestroy$ = new Subject<void>();
+  private readonly states$ = new BehaviorSubject<ServiceState>({});
+
+  /**
+   * Emits the current state of the context.
+   * - When no action has been executed, it emits `{ status: "inactive" }`.
+   * - When an action has been executed, it emits `{ status: "active", origin: unknown }`.
+   *
+   * @param context The context for which to get the state.
+   */
   state$(context: string): Observable<ContextState> {
-    return of({ status: "inactive" });
+    // Using a constant here to avoid creating a new object every time the observable emits,
+    // which causes the distinctUntilChanged operator to emit the value.
+    const inactiveState = { status: "inactive" } as ContextState;
+    return this.states$.pipe(
+      map((states) => states[context] ?? inactiveState),
+      distinctUntilChanged(),
+    );
   }
 
   /**
@@ -25,12 +52,48 @@ export class AsyncActionsService {
    * @param context A string that will be used to group the loading state of multiple async actions.
    * @param origin The object that the action originated from.
    * @param handler The function to execute.
+   * @param until An observable that will cause the action to be cancelled when it emits.
    */
-  execute(
+  async execute(
     context: string,
     origin: unknown,
     handler: FunctionReturningAwaitable,
-  ): Observable<unknown> {
-    throw new Error("Method not implemented.");
+    until?: Observable<unknown>,
+  ): Promise<void> {
+    this.updateState((state) => ({ ...state, [context]: { status: "active", origin } }));
+
+    try {
+      await firstValueFrom(
+        functionToObservable(handler).pipe(
+          takeUntil(until ? merge(this.onDestroy$, until) : this.onDestroy$),
+        ),
+      );
+    } catch (error) {
+      if (error instanceof EmptyError) {
+        // The observable completed without emitting any value, ignore it, the finally block will handle cleanup.
+        return;
+      }
+
+      throw error;
+    } finally {
+      this.removeState(context);
+    }
+  }
+
+  private updateState(handler: (state: ServiceState) => ServiceState) {
+    this.states$.next(handler(this.states$.value));
+  }
+
+  private removeState(context: string) {
+    this.updateState((state) => {
+      const newState = { ...state };
+      delete newState[context];
+      return newState;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
   }
 }
