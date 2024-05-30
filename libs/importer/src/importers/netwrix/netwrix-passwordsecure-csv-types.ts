4982b673f551ecf993a5cc78e34188b1dd781d99
Daniel James Smith <djsmith85@users.noreply.github.com>
2024-05-30 21:44:52 +0200

    Create an importer for csv-export from Netwrix Password Secure

---
+++ libs/importer/src/importers/netwrix/netwrix-passwordsecure-csv-types.ts
@@ -0,0 +1,19 @@
+// tslint:disable
+export class LoginRecord {
+  /** Organization unit / folder / collection */
+  Organisationseinheit: string;
+  /** Tags? */
+  DataTags: string;
+  /** Description/title */
+  Beschreibung: string;
+  /** Username */
+  Benutzername: string;
+  /** Password */
+  Passwort: string;
+  /** URL */
+  Internetseite: string;
+  /** Notes/additional information */
+  Informationen: string;
+  /** TOTP */
+  "One-Time Passwort": string;
+}
