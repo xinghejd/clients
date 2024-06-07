15e626601413362021faf0313671fff012032bbc
Victoria League <vleague@bitwarden.com>
2024-06-07 10:25:16 -0400

    [CL-237] Update menu styles for extension refresh (#9525)

--- libs/components/src/menu/menu-item.directive.ts
+++ libs/components/src/menu/menu-item.directive.ts
@@ -1,5 +1,6 @@
 import { FocusableOption } from "@angular/cdk/a11y";
-import { Component, ElementRef, HostBinding } from "@angular/core";
+import { coerceBooleanProperty } from "@angular/cdk/coercion";
+import { Component, ElementRef, HostBinding, Input } from "@angular/core";
 
 @Component({
   selector: "[bitMenuItem]",
@@ -9,29 +10,37 @@ export class MenuItemDirective implements FocusableOption {
   @HostBinding("class") classList = [
     "tw-block",
     "tw-w-full",
-    "tw-py-1",
-    "tw-px-4",
+    "tw-py-1.5",
+    "tw-px-3",
     "!tw-text-main",
     "!tw-no-underline",
     "tw-cursor-pointer",
     "tw-border-none",
     "tw-bg-background",
     "tw-text-left",
-    "hover:tw-bg-secondary-100",
-    "focus-visible:tw-bg-secondary-100",
+    "hover:tw-bg-primary-100",
+    "hover:!tw-underline",
     "focus-visible:tw-z-50",
     "focus-visible:tw-outline-none",
-    "focus-visible:tw-ring",
-    "focus-visible:tw-ring-offset-2",
-    "focus-visible:tw-ring-primary-700",
+    "focus-visible:tw-ring-2",
+    "focus-visible:tw-rounded-lg",
+    "focus-visible:tw-ring-inset",
+    "focus-visible:tw-ring-primary-500",
     "active:!tw-ring-0",
     "active:!tw-ring-offset-0",
     "disabled:!tw-text-muted",
     "disabled:hover:tw-bg-background",
+    "disabled:hover:!tw-no-underline",
     "disabled:tw-cursor-not-allowed",
   ];
   @HostBinding("attr.role") role = "menuitem";
   @HostBinding("tabIndex") tabIndex = "-1";
+  @HostBinding("attr.disabled")
+  get disabledAttr() {
+    return this.disabled || null; // native disabled attr must be null when false
+  }
+
+  @Input({ transform: coerceBooleanProperty }) disabled?: boolean = false;
 
   constructor(private elementRef: ElementRef) {}
 
