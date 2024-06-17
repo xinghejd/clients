b8be7d9f9c9639c04e0f58e75e4b57726b55415a
Bernd Schoolmann <mail@quexten.com>
2024-06-17 18:11:47 +0200

    Update base component

--- apps/web/src/app/auth/two-factor-auth-base.component.ts
+++ apps/web/src/app/auth/two-factor-auth-base.component.ts
@@ -1,10 +1,18 @@
-import { Directive, OnDestroy } from "@angular/core";
+import { Directive, EventEmitter, OnDestroy, Output } from "@angular/core";
+
+import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 
 @Directive()
 export class TwoFactorAuthBaseComponent implements OnDestroy {
   tokenValue: string = "";
 
-  constructor() {}
+  @Output() activeButtonTextChange = new EventEmitter<string>();
+
+  constructor(protected i18nService: I18nService) {}
 
   ngOnDestroy(): void {}
+
+  async getActionButtonText() {
+    return this.i18nService.t("continue");
+  }
 }
