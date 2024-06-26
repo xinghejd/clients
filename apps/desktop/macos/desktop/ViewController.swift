ae8853652132608dbd7415f501e50a38d0f4b18d
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-06-26 14:01:05 +0200

    feat: add macos xcode project

---
+++ apps/desktop/macos/desktop/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  desktop
+//
+//  Created by Andreas Coroiu on 2023-12-21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
