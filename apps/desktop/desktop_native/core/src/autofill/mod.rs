096cb28d57bb4b0ac6000f35e74d21930d78f71d
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-07-04 12:59:49 +0200

    feat: rename fido2 to autofill

--- apps/desktop/desktop_native/core/src/autofill/mod.rs
+++ apps/desktop/desktop_native/core/src/autofill/mod.rs
@@ -1,5 +1,5 @@
 #[cfg_attr(target_os = "linux", path = "unix.rs")]
 #[cfg_attr(target_os = "windows", path = "windows.rs")]
 #[cfg_attr(target_os = "macos", path = "macos.rs")]
-mod fido2;
-pub use fido2::*;
+mod autofill;
+pub use autofill::*;
--- apps/desktop/desktop_native/core/src/lib.rs
+++ apps/desktop/desktop_native/core/src/lib.rs
@@ -1,6 +1,6 @@
+pub mod autofill;
 pub mod biometric;
 pub mod clipboard;
 pub mod crypto;
 pub mod error;
-pub mod fido2;
 pub mod password;
