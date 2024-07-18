bf01217e3ac2e4fe5739e7b70643c6cae3462ba2
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-07-18 14:35:21 +0200

    feat: simple log that checks if autofill is enabled

--- apps/desktop/desktop_native/objc/src/native/autofill.m
+++ apps/desktop/desktop_native/objc/src/native/autofill.m
@@ -1,10 +1,28 @@
 #import <Foundation/Foundation.h>
+#import <AuthenticationServices/ASCredentialIdentityStore.h>
+#import <AuthenticationServices/ASCredentialIdentityStoreState.h>
 
 void hello_world(char* value, char* output, int output_size)
 {
   NSString *string = [[NSString alloc] initWithUTF8String:value];
-  NSLog(@"Objc from rust, hello: %@", string);
+  NSLog(@"[BW] Objc from rust, hello: %@", string);
 
   NSString *outputString = @"Hello, World from objc!";
   [outputString getCString:output maxLength:output_size encoding:NSUTF8StringEncoding];
+
+  if (@available(macos 11, *)) {
+    NSLog(@"[BW] macOS 11 or later");
+
+    ASCredentialIdentityStore *store = [ASCredentialIdentityStore sharedStore];
+    [store getCredentialIdentityStoreStateWithCompletion:^(ASCredentialIdentityStoreState * _Nonnull state) {
+        NSLog(@"[BW] state: %@", state);
+        if (state.enabled) {
+          NSLog(@"[BW] ASCredentialIdentityStore enabled");
+        } else {
+          NSLog(@"[BW] ASCredentialIdentityStore disabled");
+        }
+    }];
+  } else {
+      NSLog(@"[BW] earlier than macOS 11");
+  }
 }
