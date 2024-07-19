41a653ed51a22c0d4074ad6fbed27e42a1787892
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-07-19 14:15:43 +0200

    feat: create custom type for returning strings across rust/objc boundary

--- apps/desktop/desktop_native/objc/src/lib.rs
+++ apps/desktop/desktop_native/objc/src/lib.rs
@@ -1,20 +1,44 @@
-use std::ffi::{c_char, c_int, CStr, CString};
+use std::ffi::{c_char, CStr, CString};
 
 use anyhow::Result;
 
+#[repr(C)]
+pub struct ObjCString {
+    value: *const c_char,
+    size: usize,
+}
+
+impl From<ObjCString> for String {
+    fn from(value: ObjCString) -> Self {
+        unsafe {
+            CStr::from_ptr(value.value)
+                .to_str()
+                .expect("CStr::from_ptr failed")
+                .to_owned()
+        }
+    }
+}
+
+impl Drop for ObjCString {
+    fn drop(&mut self) {
+        unsafe {
+            free_objc_string(self);
+        }
+    }
+}
+
 extern "C" {
-    fn hello_world(value: *const c_char, output: *mut c_char, output_size: c_int);
+    // fn hello_world(value: *const c_char, output: *mut c_char, output_size: c_int) -> ObjCString;
+    fn hello_world(value: *const c_char) -> ObjCString;
+    fn free_objc_string(value: &ObjCString);
 }
 
 pub fn obj_hello_world(value: String) -> Result<String> {
     let c_value = CString::new(value).expect("CString::new failed");
     let objc_result = unsafe {
-        let c_output: *mut c_char = [0; 1024].as_mut_ptr();
-        hello_world(c_value.as_ptr(), c_output, 1024);
-        CStr::from_ptr(c_output)
-            .to_str()
-            .expect("CStr::from_ptr failed")
-            .to_owned()
+        // let c_output: *mut c_char = [0; 1024].as_mut_ptr();
+        let output = hello_world(c_value.as_ptr());
+        output.into()
     };
 
     println!(
--- apps/desktop/desktop_native/objc/src/native/autofill.m
+++ apps/desktop/desktop_native/objc/src/native/autofill.m
@@ -13,13 +13,38 @@
 
 // check https://barhamon.com/post/rust_and_nsstring.html
 
-void hello_world(char* value, char* output, int output_size)
+/// Simple struct to hold a C-string and its length
+/// This is used to return strings created in Objective-C to Rust
+/// so that Rust can free the memory when it's done with the string
+struct ObjCString {
+  char* value;
+  size_t size;
+};
+
+struct ObjCString nsstring_to_obj_c_string(NSString* string) {
+  size_t size = [string lengthOfBytesUsingEncoding:NSUTF8StringEncoding] + 1;
+  char *value = malloc(size);
+  [string getCString:value maxLength:size encoding:NSUTF8StringEncoding];
+
+  struct ObjCString obj_c_string;
+  obj_c_string.value = value;
+  obj_c_string.size = size;
+
+  return obj_c_string;
+}
+
+void free_objc_string(struct ObjCString *value) {
+  free(value->value);
+}
+
+// void hello_world(char* value, char* output, int output_size)
+struct ObjCString hello_world(char* value)
 {
-  NSString *string = [[NSString alloc] initWithUTF8String:value];
-  NSLog(@"[BW] Objc from rust, hello: %@", string);
+  NSString *inputString = [[NSString alloc] initWithUTF8String:value];
+  NSLog(@"[BW] Objc from rust, hello: %@", inputString);
 
   NSString *outputString = @"Hello, World from objc!";
-  [outputString getCString:output maxLength:output_size encoding:NSUTF8StringEncoding];
+  // [outputString getCString:output maxLength:output_size encoding:NSUTF8StringEncoding];
 
   if (@available(macos 14, *))
   {
@@ -41,4 +66,8 @@ void hello_world(char* value, char* output, int output_size)
   else {
     NSLog(@"[BW] macOS 10 or earlier - no autofill support");
   }
+
+  [inputString dealloc];
+
+  return nsstring_to_obj_c_string(outputString);
 }
