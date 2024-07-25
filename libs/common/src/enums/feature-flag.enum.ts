85fc3961d9f13d83d4cef9241d93192f746cc0e2
Ike <137194738+ike-kottlowski@users.noreply.github.com>
2024-07-25 08:13:57 -0700

    added feature flag, and services for tokenable when adding Authenticator two factor; (#10261)

--- libs/common/src/enums/feature-flag.enum.ts
+++ libs/common/src/enums/feature-flag.enum.ts
@@ -28,6 +28,7 @@ export enum FeatureFlag {
   AC2828_ProviderPortalMembersPage = "AC-2828_provider-portal-members-page",
   AutoSubmitLogin = "auto-submit-login",
   DeviceTrustLogging = "pm-8285-device-trust-logging",
+  AuthenticatorTwoFactorToken = "authenticator-2fa-token",
 }
 
 export type AllowedFeatureFlagTypes = boolean | number | string;
@@ -66,6 +67,7 @@ export const DefaultFeatureFlagValue = {
   [FeatureFlag.AC2828_ProviderPortalMembersPage]: FALSE,
   [FeatureFlag.AutoSubmitLogin]: FALSE,
   [FeatureFlag.DeviceTrustLogging]: FALSE,
+  [FeatureFlag.AuthenticatorTwoFactorToken]: FALSE,
 } satisfies Record<FeatureFlag, AllowedFeatureFlagTypes>;
 
 export type DefaultFeatureFlagValueType = typeof DefaultFeatureFlagValue;
