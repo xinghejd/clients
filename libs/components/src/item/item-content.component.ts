45542438cfd276b1294b65d70081ce1180a8327c
Victoria League <vleague@bitwarden.com>
2024-08-01 10:29:35 -0400

    [CL-338] Remove extra space in item content when end slot is empty (#10350)

--- libs/components/src/item/item-content.component.ts
+++ libs/components/src/item/item-content.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import {
+  AfterContentChecked,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  signal,
+  ViewChild,
+} from "@angular/core";
 
 @Component({
   selector: "bit-item-content, [bit-item-content]",
@@ -12,4 +19,12 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
   },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ItemContentComponent {}
+export class ItemContentComponent implements AfterContentChecked {
+  @ViewChild("endSlot") endSlot: ElementRef<HTMLDivElement>;
+
+  protected endSlotHasChildren = signal(false);
+
+  ngAfterContentChecked(): void {
+    this.endSlotHasChildren.set(this.endSlot?.nativeElement.childElementCount > 0);
+  }
+}
