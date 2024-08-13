ede8177049382167a70a17c656361f70ecdaaff3
Jared Snider <jsnider@bitwarden.com>
2024-08-13 16:55:51 -0400

    PM-9449 - UnlockOptions - replace incorrect type

--- libs/auth/src/angular/lock/lock-component.service.ts
+++ libs/auth/src/angular/lock/lock-component.service.ts
@@ -12,7 +12,7 @@ export type UnlockOptions = {
   masterPasswordEnabled: boolean;
   pinEnabled: boolean;
   biometrics: {
-    enabled: false;
+    enabled: boolean;
     disableReason: BiometricsDisableReason | null;
   };
 };
