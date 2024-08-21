d9b77d8b0535874f6a3f24a35407b029d8d49e74
Victoria League <vleague@bitwarden.com>
2024-08-21 14:27:49 -0400

    [CL-380] Remove hover state from disabled form fields (#10639)

--- libs/components/src/form-field/form-field.component.ts
+++ libs/components/src/form-field/form-field.component.ts
@@ -49,6 +49,9 @@ export class BitFormFieldComponent implements AfterContentChecked {
       this.input.hasError
         ? "group-hover/bit-form-field:tw-border-danger-700"
         : "group-hover/bit-form-field:tw-border-primary-500",
+      // the next 2 selectors override the above hover selectors when the input (or text area) is non-interactive (i.e. readonly, disabled)
+      "group-has-[input:read-only]/bit-form-field:group-hover/bit-form-field:tw-border-secondary-500",
+      "group-has-[textarea:read-only]/bit-form-field:group-hover/bit-form-field:tw-border-secondary-500",
       "group-focus-within/bit-form-field:tw-outline-none",
       shouldFocusBorderAppear ? "group-focus-within/bit-form-field:tw-border-2" : "",
       shouldFocusBorderAppear ? "group-focus-within/bit-form-field:tw-border-primary-500" : "",
