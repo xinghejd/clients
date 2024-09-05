bed3a28215ef29a3ce16862d33bcccaa0d2ee5b2
Cy Okeke <cokeke@bitwarden.com>
2024-09-05 12:07:55 +0100

    Resolve lint error

--- bitwarden_license/bit-web/src/app/secrets-manager/overview/overview.module.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/overview/overview.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from "@angular/core";
 
+import { BannerModule } from "@bitwarden/components";
+
 import { OnboardingModule } from "../../../../../../apps/web/src/app/shared/components/onboarding/onboarding.module";
 import { SecretsManagerSharedModule } from "../shared/sm-shared.module";
 
 import { OverviewRoutingModule } from "./overview-routing.module";
 import { OverviewComponent } from "./overview.component";
 import { SectionComponent } from "./section.component";
-import { BannerModule } from "@bitwarden/components";
 
 @NgModule({
   imports: [SecretsManagerSharedModule, OverviewRoutingModule, OnboardingModule, BannerModule],
