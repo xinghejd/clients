c56fe9c2c972a51145d92fa141d083822b50c394
Daniel García <dani-garcia@users.noreply.github.com>
2024-09-18 18:32:11 +0200

    Remove client reconnections

--- apps/desktop/desktop_native/core/src/ipc/client.rs
+++ apps/desktop/desktop_native/core/src/ipc/client.rs
@@ -1,17 +1,11 @@
-use std::{
-    path::{Path, PathBuf},
-    time::Duration,
-};
+use std::path::PathBuf;
 
 use interprocess::local_socket::{
     tokio::{prelude::*, Stream},
     GenericFilePath, ToFsName,
 };
 use log::{error, info};
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    time::sleep,
-};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 use crate::ipc::NATIVE_MESSAGING_BUFFER_SIZE;
 
@@ -19,32 +13,6 @@ pub async fn connect(
     path: PathBuf,
     send: tokio::sync::mpsc::Sender<String>,
     mut recv: tokio::sync::mpsc::Receiver<String>,
-) {
-    // Keep track of connection failures to make sure we don't leave the process as a zombie
-    let mut connection_failures = 0;
-
-    loop {
-        match connect_inner(&path, &send, &mut recv).await {
-            Ok(()) => return,
-            Err(e) => {
-                connection_failures += 1;
-                if connection_failures >= 20 {
-                    error!("Failed to connect to IPC server after 20 attempts: {e}");
-                    return;
-                }
-
-                error!("Failed to connect to IPC server: {e}");
-            }
-        }
-
-        sleep(Duration::from_secs(5)).await;
-    }
-}
-
-async fn connect_inner(
-    path: &Path,
-    send: &tokio::sync::mpsc::Sender<String>,
-    recv: &mut tokio::sync::mpsc::Receiver<String>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     info!("Attempting to connect to {}", path.display());
 
