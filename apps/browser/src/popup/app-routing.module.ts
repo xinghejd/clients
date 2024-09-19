583646f2893fbc362b66a167a6f00cb394e99ece
Jared Snider <jsnider@bitwarden.com>
2024-09-19 16:11:56 -0400

    PM-9449 - Extension AppRoutingModule - LockV2 should use ExtensionAnonLayoutWrapperData

--- apps/browser/src/popup/app-routing.module.ts
+++ apps/browser/src/popup/app-routing.module.ts
@@ -33,7 +33,10 @@ import { twofactorRefactorSwap } from "../../../../libs/angular/src/utils/two-fa
 import { fido2AuthGuard } from "../auth/guards/fido2-auth.guard";
 import { AccountSwitcherComponent } from "../auth/popup/account-switching/account-switcher.component";
 import { EnvironmentComponent } from "../auth/popup/environment.component";
-import { ExtensionAnonLayoutWrapperComponent } from "../auth/popup/extension-anon-layout-wrapper/extension-anon-layout-wrapper.component";
+import {
+  ExtensionAnonLayoutWrapperComponent,
+  ExtensionAnonLayoutWrapperData,
+} from "../auth/popup/extension-anon-layout-wrapper/extension-anon-layout-wrapper.component";
 import { HintComponent } from "../auth/popup/hint.component";
 import { HomeComponent } from "../auth/popup/home.component";
 import { LockComponent } from "../auth/popup/lock.component";
@@ -433,7 +436,8 @@ const routes: Routes = [
           pageIcon: LockIcon,
           pageTitle: "yourVaultIsLockedV2",
           showReadonlyHostname: true,
-        } satisfies AnonLayoutWrapperData,
+          showAcctSwitcher: true,
+        } satisfies ExtensionAnonLayoutWrapperData,
         children: [
           {
             path: "",
