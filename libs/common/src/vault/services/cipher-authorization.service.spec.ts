f3882ba1fd28607eba3184a47f1bd47f61c24f67
gbubemismith <gsmithwalter@gmail.com>
2024-09-27 15:55:07 -0400

    Modified service to cater for org vault

--- libs/common/src/vault/services/cipher-authorization.service.spec.ts
+++ libs/common/src/vault/services/cipher-authorization.service.spec.ts
@@ -1,6 +1,8 @@
 import { mock } from "jest-mock-extended";
 import { of } from "rxjs";
 
+import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
+import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
 import { CollectionId } from "@bitwarden/common/types/guid";
 
 import { CollectionService } from "../abstractions/collection.service";
@@ -16,11 +18,17 @@ describe("CipherAuthorizationService", () => {
   let cipherAuthorizationService: CipherAuthorizationServiceAbstraction;
 
   const mockCollectionService = mock<CollectionService>();
+  const mockOrganizationService = mock<OrganizationService>();
 
   // Mock factories
-  const createMockCipher = (organizationId: string | null, collectionIds: string[]) => ({
+  const createMockCipher = (
+    organizationId: string | null,
+    collectionIds: string[],
+    edit: boolean = true,
+  ) => ({
     organizationId,
     collectionIds,
+    edit,
   });
 
   const createMockCollection = (id: string, manage: boolean) => ({
@@ -28,8 +36,22 @@ describe("CipherAuthorizationService", () => {
     manage,
   });
 
+  const createMockOrganization = (
+    permissions: { editAnyCollection: boolean },
+    allowAdminAccessToAllCollectionItems: boolean,
+    isAdmin: boolean,
+  ) => ({
+    permissions,
+    allowAdminAccessToAllCollectionItems,
+    isAdmin,
+  });
+
   beforeEach(() => {
-    cipherAuthorizationService = new CipherAuthorizationService(mockCollectionService);
+    jest.clearAllMocks();
+    cipherAuthorizationService = new CipherAuthorizationService(
+      mockCollectionService,
+      mockOrganizationService,
+    );
   });
 
   it("should return true if cipher has no organizationId", (done) => {
@@ -41,9 +63,44 @@ describe("CipherAuthorizationService", () => {
     });
   });
 
+  it("should return true if cipher is in unassigned collection", (done) => {
+    const cipher = createMockCipher("org1", []) as CipherView;
+
+    cipherAuthorizationService.canDeleteCipher$(cipher).subscribe((result) => {
+      expect(result).toBe(true);
+      done();
+    });
+  });
+
+  it("should return true if user has editAnyCollection permission in the org", (done) => {
+    const cipher = createMockCipher("org1", ["col1"]) as CipherView;
+    const organization = createMockOrganization({ editAnyCollection: true }, false, false);
+    mockOrganizationService.get$.mockReturnValue(of(organization as Organization));
+
+    cipherAuthorizationService.canDeleteCipher$(cipher).subscribe((result) => {
+      expect(result).toBe(true);
+      expect(mockOrganizationService.get$).toHaveBeenCalledWith("org1");
+      done();
+    });
+  });
+
+  it("should return true if admin access is enabled and user is admin", (done) => {
+    const cipher = createMockCipher("org1", ["col1"]) as CipherView;
+    const org = createMockOrganization({ editAnyCollection: false }, true, true);
+    mockOrganizationService.get$.mockReturnValue(of(org as Organization));
+
+    cipherAuthorizationService.canDeleteCipher$(cipher).subscribe((result) => {
+      expect(result).toBe(true);
+      expect(mockOrganizationService.get$).toHaveBeenCalledWith("org1");
+      done();
+    });
+  });
+
   it("should return true if activeCollectionId is provided and has manage permission", (done) => {
     const cipher = createMockCipher("org1", ["col1", "col2"]) as CipherView;
     const activeCollectionId = "col1" as CollectionId;
+    const org = createMockOrganization({ editAnyCollection: false }, false, false);
+    mockOrganizationService.get$.mockReturnValue(of(org as Organization));
 
     const allCollections = [
       createMockCollection("col1", true),
@@ -66,6 +123,8 @@ describe("CipherAuthorizationService", () => {
   it("should return false if activeCollectionId is provided and manage permission is not present", (done) => {
     const cipher = createMockCipher("org1", ["col1", "col2"]) as CipherView;
     const activeCollectionId = "col1" as CollectionId;
+    const org = createMockOrganization({ editAnyCollection: false }, false, false);
+    mockOrganizationService.get$.mockReturnValue(of(org as Organization));
 
     const allCollections = [
       createMockCollection("col1", false),
@@ -87,6 +146,8 @@ describe("CipherAuthorizationService", () => {
 
   it("should return true if any collection has manage permission", (done) => {
     const cipher = createMockCipher("org1", ["col1", "col2", "col3"]) as CipherView;
+    const org = createMockOrganization({ editAnyCollection: false }, false, false);
+    mockOrganizationService.get$.mockReturnValue(of(org as Organization));
 
     const allCollections = [
       createMockCollection("col1", false),
@@ -110,6 +171,8 @@ describe("CipherAuthorizationService", () => {
 
   it("should return false if no collection has manage permission", (done) => {
     const cipher = createMockCipher("org1", ["col1", "col2"]) as CipherView;
+    const org = createMockOrganization({ editAnyCollection: false }, false, false);
+    mockOrganizationService.get$.mockReturnValue(of(org as Organization));
 
     const allCollections = [
       createMockCollection("col1", false),
--- libs/common/src/vault/services/cipher-authorization.service.ts
+++ libs/common/src/vault/services/cipher-authorization.service.ts
@@ -1,5 +1,6 @@
-import { map, Observable, of } from "rxjs";
+import { map, Observable, of, switchMap } from "rxjs";
 
+import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
 import { CollectionId } from "@bitwarden/common/types/guid";
 
 import { CollectionService } from "../abstractions/collection.service";
@@ -24,32 +25,46 @@ export abstract class CipherAuthorizationServiceAbstraction {
  * {@link CipherAuthorizationServiceAbstraction}
  */
 export class CipherAuthorizationService implements CipherAuthorizationServiceAbstraction {
-  constructor(private collectionService: CollectionService) {}
+  constructor(
+    private collectionService: CollectionService,
+    private organizationService: OrganizationService,
+  ) {}
 
   /**
    *
    * {@link CipherAuthorizationServiceAbstraction.canDeleteCipher$}
    */
   canDeleteCipher$(cipher: CipherView, activeCollectionId?: CollectionId): Observable<boolean> {
-    if (cipher.organizationId == null) {
-      return of(true);
+    if (cipher.organizationId == null || cipher.collectionIds?.length === 0) {
+      return of(cipher.edit);
     }
 
-    return this.collectionService
-      .decryptedCollectionViews$(cipher.collectionIds as CollectionId[])
-      .pipe(
-        map((allCollections) => {
-          if (activeCollectionId) {
-            const activeCollection = allCollections.find((c) => c.id === activeCollectionId);
-            if (activeCollection) {
-              return activeCollection.manage === true;
-            }
-          }
+    return this.organizationService.get$(cipher.organizationId).pipe(
+      switchMap((organization) => {
+        if (
+          organization?.permissions.editAnyCollection ||
+          (organization?.allowAdminAccessToAllCollectionItems && organization.isAdmin)
+        ) {
+          return of(true);
+        }
 
-          return allCollections
-            .filter((c) => cipher.collectionIds.includes(c.id))
-            .some((collection) => collection.manage);
-        }),
-      );
+        return this.collectionService
+          .decryptedCollectionViews$(cipher.collectionIds as CollectionId[])
+          .pipe(
+            map((allCollections) => {
+              if (activeCollectionId) {
+                const activeCollection = allCollections.find((c) => c.id === activeCollectionId);
+                if (activeCollection) {
+                  return activeCollection.manage === true;
+                }
+              }
+
+              return allCollections
+                .filter((c) => cipher.collectionIds.includes(c.id))
+                .some((collection) => collection.manage);
+            }),
+          );
+      }),
+    );
   }
 }
