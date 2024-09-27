f0074a028c4e9a768413b57ce7a88d67f8eef06f
gbubemismith <gsmithwalter@gmail.com>
2024-09-27 10:27:45 -0400

    Added comments to the service

--- libs/common/src/vault/services/cipher-authorization.service.ts
+++ libs/common/src/vault/services/cipher-authorization.service.ts
@@ -5,13 +5,31 @@ import { CollectionId } from "@bitwarden/common/types/guid";
 import { CollectionService } from "../abstractions/collection.service";
 import { CipherView } from "../models/view/cipher.view";
 
+/**
+ * Service for managing user cipher authorization.
+ */
 export abstract class CipherAuthorizationServiceAbstraction {
+  /**
+   * Determines if the user can delete the specified cipher.
+   *
+   * @param {CipherView} cipher - The cipher used to determine if the user can delete it.
+   * @param {CollectionId} [activeCollectionId] - Optional. The selected collection id from the vault filter.
+   *
+   * @returns {Observable<boolean>} - An observable that emits a boolean value indicating if the user can delete the cipher.
+   */
   canDeleteCipher$: (cipher: CipherView, activeCollectionId?: CollectionId) => Observable<boolean>;
 }
 
+/**
+ * {@link CipherAuthorizationServiceAbstraction}
+ */
 export class CipherAuthorizationService implements CipherAuthorizationServiceAbstraction {
   constructor(private collectionService: CollectionService) {}
 
+  /**
+   *
+   * {@link CipherAuthorizationServiceAbstraction.canDeleteCipher$}
+   */
   canDeleteCipher$(cipher: CipherView, activeCollectionId?: CollectionId): Observable<boolean> {
     if (cipher.organizationId == null) {
       return of(true);
